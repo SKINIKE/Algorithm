532b0a70498f14779f0df3b154fb2cf6a2a96025
SKINIKE <83528236+SKINIKE@users.noreply.github.com>
2023-11-12 16:00:24 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+
+  for (int i = 1; i < a + 1; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
